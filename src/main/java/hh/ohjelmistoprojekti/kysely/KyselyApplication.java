023785b7a426d4f61bbd1b2480950172b4e0c404
poddui <jimi8888.podduikin@gmail.com>
2023-04-13 08:23:57 +0300

    Added more test data

--- src/main/java/hh/ohjelmistoprojekti/kysely/KyselyApplication.java
+++ src/main/java/hh/ohjelmistoprojekti/kysely/KyselyApplication.java
@@ -24,7 +24,11 @@ public class KyselyApplication {
 	public CommandLineRunner demo(PollRepository prepository, QuestionRepository qrepository) 
 	{return (args) -> {
         Poll p1 = new Poll("banaani kysely");
+        Poll p2 = new Poll("vaate kysely");
+        Poll p3 = new Poll("lempiruoka kysely");
         prepository.save(p1);
+        prepository.save(p2);
+        prepository.save(p3);
 
         Question q1 = new Question();
         q1.setQuery("Onko banaani keltainen?");
@@ -41,10 +45,34 @@ public class KyselyApplication {
         q3.setPoll(p1);
         qrepository.save(q3);
         
-        prepository.save(p1);
+        
+        Question q4 = new Question();
+        q4.setQuery("Kengänkoko?");
+        q4.setPoll(p2);
+        qrepository.save(q4);
 
-        System.out.println(p1);
-        System.out.println(q1);
+        Question q5 = new Question();
+        q5.setQuery("Pituus?");
+        q5.setPoll(p2);
+        qrepository.save(q5);
+
+        Question q6 = new Question();
+        q6.setQuery("Vaatekoko?");
+        q6.setPoll(p2);
+        qrepository.save(q6);
+        
+        
+        Question q7 = new Question();
+        q7.setQuery("Mikä on lempijuomasi?");
+        q7.setPoll(p3);
+        qrepository.save(q7);
+
+        Question q8 = new Question();
+        q8.setQuery("Mikä on lempiruokasi?");
+        q8.setPoll(p3);
+        qrepository.save(q8);
+
+
 	};
 
 	
