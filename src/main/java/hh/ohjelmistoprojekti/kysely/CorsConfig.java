d4c1e76a62ab86bbfa90c40d850d0c4245920dab
poddui <104581021+poddui@users.noreply.github.com>
2023-04-18 09:02:24 +0300

    Fixed CorsConfig

--- src/main/java/hh/ohjelmistoprojekti/kysely/CorsConfig.java
+++ src/main/java/hh/ohjelmistoprojekti/kysely/CorsConfig.java
@@ -1,10 +1,7 @@
 package hh.ohjelmistoprojekti.kysely;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
